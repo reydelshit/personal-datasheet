cd616f6e82ac2ba52fcd726fbbf2b8394fdf1184
Reydel Ocon <reydel321@gmail.com>
2022-06-15 11:19:00 +0800

    ghahaH

---
+++ print.js
@@ -0,0 +1,8 @@
+const getPrintBtn = document.querySelector('.printBTN');
+
+// console.log(getPrintBtn)
+
+getPrintBtn.addEventListener('click', () => {
+    // console.log('heha')
+    window.print();
+})
